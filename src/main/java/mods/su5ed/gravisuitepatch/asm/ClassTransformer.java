10811cd2db5c17d439ea38caf54801c7cc4637e9
Su5eD <adamko.zachar@gmail.com>
2021-07-06 23:40:42 +0200

    Fix a NPE in ItemAdvancedDrill::getBrokenBlocks

--- src/main/java/mods/su5ed/gravisuitepatch/asm/ClassTransformer.java
+++ src/main/java/mods/su5ed/gravisuitepatch/asm/ClassTransformer.java
@@ -1,10 +1,7 @@
 package mods.su5ed.gravisuitepatch.asm;
 
 import net.minecraft.launchwrapper.IClassTransformer;
-import org.objectweb.asm.ClassReader;
-import org.objectweb.asm.ClassVisitor;
-import org.objectweb.asm.ClassWriter;
-import org.objectweb.asm.MethodVisitor;
+import org.objectweb.asm.*;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -23,7 +20,7 @@ public class ClassTransformer implements IClassTransformer {
     public byte[] transform(String name, String transformedName, byte[] bytes) {
         if (TRANSFORMERS.containsKey(name)) {
             ClassReader reader = new ClassReader(bytes);
-            ClassWriter writer = new ClassWriter(reader, 0);
+            ClassWriter writer = new ClassWriter(reader, ClassWriter.COMPUTE_FRAMES);
             
             reader.accept(TRANSFORMERS.get(name).apply(ASM4, writer), 0);
             
@@ -37,6 +34,14 @@ public class ClassTransformer implements IClassTransformer {
         
         public ItemAdvancedDrillVisitor(int api, ClassVisitor cv) {
             super(api, cv);
+        }
+
+        @Override
+        public MethodVisitor visitMethod(int access, String name, String desc, String signature, String[] exceptions) {
+            if (name.equals("getBrokenBlocks") && desc.equals("(Lnet/minecraft/entity/player/EntityPlayer;Lnet/minecraft/util/math/RayTraceResult;)Ljava/util/Collection;")) {
+                return new GetBrokenBlocksVisitor(ASM4, super.visitMethod(access, name, desc, signature, exceptions));
+            }
+            return super.visitMethod(access, name, desc, signature, exceptions);
         }
 
         @Override
@@ -72,6 +77,27 @@ public class ClassTransformer implements IClassTransformer {
             mv.visitMethodInsn(INVOKESTATIC, "mods/su5ed/gravisuitepatch/asm/ASMHooks", targetName, desc, false);
             mv.visitInsn(IRETURN);
             mv.visitEnd();
+        }
+        
+        private static class GetBrokenBlocksVisitor extends MethodVisitor {
+            
+            public GetBrokenBlocksVisitor(int api, MethodVisitor mv) {
+                super(api, mv);
+            }
+
+            @Override
+            public void visitCode() {
+                Label label = new Label();
+                visitVarInsn(ALOAD, 1);
+                visitJumpInsn(IFNONNULL, label);
+                visitTypeInsn(NEW, "java/util/HashSet");
+                visitInsn(DUP);
+                visitMethodInsn(INVOKESPECIAL, "java/util/HashSet", "<init>", "()V", false);
+                visitInsn(ARETURN);
+                visitLabel(label);
+                
+                super.visitCode();
+            }
         }
     }
 }
